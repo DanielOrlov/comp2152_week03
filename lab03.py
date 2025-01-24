0f9f1efb4cb3d1e5b24906429efd5dd9f48d27bc
Daniel <daniil.orlov.ua@gmail.com>
2025-01-24 09:26:55 -0500

    added weapons

--- lab03.py
+++ lab03.py
@@ -1,1 +1,8 @@
-print("Hello, World!")
+# Dice options using list() and range()
+diceOptions = list (range(1, 7))
+
+# Weapons Array
+weapons = ["Fist", "Knife", "Club", "Gun", "Bomb", "Nuclear Bomb"]
+
+# Display available weapons
+print("Available weapons: ", ', '.join(weapons))
