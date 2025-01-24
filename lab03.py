68530027dea4950d42e0b47d6686d0e2448ea592
Daniel <daniil.orlov.ua@gmail.com>
2025-01-24 09:46:49 -0500

    final commit of the lab 3

--- lab03.py
+++ lab03.py
@@ -1,3 +1,4 @@
+import random
 # Dice options using list() and range()
 diceOptions = list (range(1, 7))
 
@@ -20,3 +21,28 @@ def get_combat_strenght(prompt):
 
 combatStrength = get_combat_strenght("Enter your combat strength (1-6): ")
 mCombatStrength = get_combat_strenght("Enter monster's combat strength (1-6): ")
+
+for j in range(1, 21, 2):
+    heroRoll = random.choice(diceOptions)
+    monsterRoll = random.choice(diceOptions)
+
+    heroWeapon = weapons[heroRoll - 1]
+    monsterWeapon = weapons[monsterRoll - 1]
+
+    heroTotal = combatStrength + heroRoll
+    monsterTotal = mCombatStrength + monsterRoll
+
+    print(f"\n Hero rolled {heroRoll}, Monster rolled {monsterRoll}")
+    print(f"\n Hero selected {heroWeapon}, Monster selecter {monsterWeapon}")
+    print(f"\n Hero total strength: {heroTotal}, Monster total strength: {monsterTotal}")
+
+    #Determine the winner
+    if heroTotal > monsterTotal :
+        print("Hero wins")
+    elif heroRoll < monsterTotal :
+        print("Monster wins")
+    else:
+        print("It's a tie!")
+
+if j != 11:
+    print("\n Battle concluded after 20 rounds")
