177ba2a674cf591c614a85de3ae18f1d72e1a86d
Daniel <daniil.orlov.ua@gmail.com>
2025-01-24 09:32:03 -0500

    added strength input

--- lab03.py
+++ lab03.py
@@ -6,3 +6,17 @@ weapons = ["Fist", "Knife", "Club", "Gun", "Bomb", "Nuclear Bomb"]
 
 # Display available weapons
 print("Available weapons: ", ', '.join(weapons))
+
+def get_combat_strenght(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            if 1 <= value <= 6:
+                return value
+            else:
+                print("Invalid input! Please enter a number between 1-6.")
+        except ValueError:
+            print("Invalid input. Please enter a number between 1-6")
+
+combatStrength = get_combat_strenght("Enter your combat strength (1-6): ")
+mCombatStrength = get_combat_strenght("Enter monster's combat strength (1-6): ")
